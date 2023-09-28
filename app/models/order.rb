206b41aa545a7b2ecb971e08c7f1bb68911a642a
Luis Alvarez Lopez <luis30n@gmail.com>
2023-09-28 21:50:46 +0200

    [core] Define undisbursed scope

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@
 class Order < ApplicationRecord
   belongs_to :merchant
   belongs_to :disbursement, optional: true
+
+  scope :undisbursed, -> { where(disbursement_id: nil) }
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe Order, type: :model do
     it { is_expected.to belong_to(:disbursement).optional(true) }
     it { is_expected.to belong_to(:merchant) }
   end
+
+  describe '.undisbursed' do
+    let!(:disbursed_orders) { create_list(:order, 3, disbursement: build(:disbursement)) }
+    context 'when some orders are not disbursed' do
+      let!(:undisbursed_orders) { create_list(:order, 2, disbursement: nil) }
+
+      it 'returns the undisbursed orders' do
+        expect(described_class.undisbursed).to eq(undisbursed_orders)
+      end
+    end
+
+    context 'when all orders are disbursed' do
+      it 'returns an empty collection' do
+        expect(described_class.undisbursed).to be_empty
+      end
+    end
+  end
 end
