cee6769045868c9f10359068aea9d75b4658e683
Luis Alvarez Lopez <luis30n@gmail.com>
2023-09-28 18:18:55 +0200

    [db] Define orders table

---
+++ db/migrate/20230928161714_create_orders.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.references :merchant, null: false, foreign_key: true
+      t.references :disbursement, null: false, foreign_key: true
+      t.decimal :amount, precision: 10, scale: 2
+
+      t.timestamps
+    end
+  end
+end
