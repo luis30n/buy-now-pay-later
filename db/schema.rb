e2a7304d463c3d3c6711697f0a72e083a0030a8e
Luis Alvarez Lopez <luis30n@gmail.com>
2023-09-28 18:03:04 +0200

    [db] Define disbursements table

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_927_153_350) do
+ActiveRecord::Schema[7.0].define(version: 20_230_928_160_042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'disbursements', force: :cascade do |t|
+    t.string 'reference'
+    t.decimal 'amount', precision: 10, scale: 2
+    t.bigint 'merchant_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['merchant_id'], name: 'index_disbursements_on_merchant_id'
+  end
 
   create_table 'merchants', force: :cascade do |t|
     t.string 'reference'
@@ -25,4 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 20_230_927_153_350) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
+
+  add_foreign_key 'disbursements', 'merchants'
 end
