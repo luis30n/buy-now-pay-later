4d4e808a05db362177f4058d32af8ccc3cb3978f
Luis Alvarez Lopez <luis30n@gmail.com>
2023-10-01 17:22:21 +0200

    [fix] Autogenerate uuid references for disbursements

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_930_092_117) do
+ActiveRecord::Schema[7.0].define(version: 20_231_001_150_954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+  enable_extension 'uuid-ossp'
 
   create_table 'disbursements', force: :cascade do |t|
     t.decimal 'amount', precision: 10, scale: 2
     t.bigint 'merchant_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.uuid 'reference'
+    t.uuid 'reference', default: -> { 'uuid_generate_v4()' }
     t.index ['merchant_id'], name: 'index_disbursements_on_merchant_id'
   end
 
